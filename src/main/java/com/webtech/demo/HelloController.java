84f326d5dd015edea1f70cde56c5d2b2e249e3d1
giogreg <50664701+giogreg@users.noreply.github.com>
2021-05-06 20:46:54 +0200

    Giogreg patch (#7)
    * added ci.yml
    
    * test push
    
    * modified gradlew
    
    * added ci.yml
    
    * JDK 11 to 16
    
    * added database, http endpoints and service
    
    * some small adjustments fpr the redirection
    
    * update
    
    * changed UrlRestController to HelloController
    
    Co-authored-by: Gregor <nichtgregor@gmail.com>

--- src/main/java/com/webtech/demo/HelloController.java
+++ src/main/java/com/webtech/demo/HelloController.java
@@ -1,17 +1,75 @@
 package com.webtech.demo;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.webtech.demo.model.Url;
+import com.webtech.demo.service.UrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.logging.Logger;
 
 @RestController
 public class HelloController {
 
+    // Create a Logger
+    Logger logger = Logger.getLogger(HelloController.class.getName());
+
+    @Autowired
+    private UrlService urlService;
+
     @RequestMapping("/")
-    public String landing() { return "Hello World!"; }
+    public String landing() { return "Landing Page"; }
 
     @RequestMapping("/login")
     public String login() {
         return "Login Page";
     }
 
+    @PostMapping(value="/generate", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Url> generateShortLink(@RequestBody Url longUrl){
+        try{
+            Url resUrl = urlService.generateUrl(longUrl);
+            logger.info(resUrl.toString());
+            return new ResponseEntity<Url>(resUrl, HttpStatus.CREATED);
+        }
+        catch(NullPointerException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping(value="/generateexpired", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Url> generateUrl30daysValid(@RequestBody Url longUrl){
+        try{
+            Url resUrl = urlService.generateUrl(longUrl);
+            logger.info(resUrl.toString());
+            return new ResponseEntity<Url>(resUrl, HttpStatus.CREATED);
+        }
+        catch(NullPointerException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/{shortUrl}")
+    public RedirectView redirectToLongUrl(@PathVariable String shortUrl){
+        Url resUrl = urlService.findByShortUrl(shortUrl);
+        logger.info(resUrl.toString());
+        if (urlService.isUrlValid(resUrl)) {
+            RedirectView redirectView = new RedirectView();
+            redirectView.setUrl(resUrl.getLongUrl());
+            return redirectView;
+        }
+        return null;
+    }
+
+    @GetMapping("/unvalid/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Url> unvalid(@PathVariable long id){
+        Url resUrl = urlService.setGueltigBis(id);
+        return new ResponseEntity<Url>(resUrl, HttpStatus.CREATED);
+    }
 }
--- src/test/java/com/webtech/demo/ApplicationTests.java
+++ src/test/java/com/webtech/demo/ApplicationTests.java
@@ -1,13 +1,8 @@
 package com.webtech.demo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
