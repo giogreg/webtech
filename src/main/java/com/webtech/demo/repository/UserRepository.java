fe0b591a21d433c3cda59caea6ffab4481a5380d
jmancook54 <82171672+jmancook54@users.noreply.github.com>
2021-05-10 16:41:12 +0200

    Update UserRepository.java

--- src/main/java/com/webtech/demo/repository/UserRepository.java
+++ src/main/java/com/webtech/demo/repository/UserRepository.java
@@ -4,7 +4,7 @@ import com.webtech.demo.model.Url;
 import com.webtech.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Url, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     public User findUserById(long id);
 
